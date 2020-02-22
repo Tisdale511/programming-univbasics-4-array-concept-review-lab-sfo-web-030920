31f2bd1d266126390e0ec6caa77bf5fa7e601842
Tisdale Fry <tisdalefry@gmail.com>
2020-02-22 02:42:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,14 +8,6 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min_val = array[0]
-  i = 0
-  while array.length < i do
-    if array[i] < min_val
-      min_val = array[i]
-    else
-      next
-    end
-  end
-  return min_val
+  new_arr = array.sort
+  return new_arr[0]
 end
